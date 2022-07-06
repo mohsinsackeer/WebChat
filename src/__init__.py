c9c3859e56bd8d55af7a6f55ee13708426370933
M Mohsin Sackeer <mohsin.ms97@gmail.com>
2022-07-06 15:19:33 +0530

    v0.0.1: Created a base for the app. Login and Signup now active. Shows you 'Success' if you try to do either of these action

---
+++ src/__init__.py
@@ -0,0 +1,70 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+db = SQLAlchemy()
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    username = db.Column(db.String(80), index=True, unique=True)
+    email = db.Column(db.String(80), index=True, unique=True)
+    password_hash = db.Column(db.String(80), index=True)
+    # image = db.Column(db.String(256))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, entered_password):
+        return check_password_hash(self.password_hash, entered_password)
+    
+    def get_json_data(self):
+        return {
+            "username" : self.username,
+            "name"  : self.name,
+            "email" : self.email
+        }
+
+
+def create_app():
+    curr_dir = os.getcwd()
+    database_path = os.path.join(curr_dir, 'database/db.sqlite')
+    print(f'Database Path: {database_path}')
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'NoOneWillWillEverFigureOut.Ever!'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + database_path
+    login_manager = LoginManager()
+
+    with app.app_context():
+        db.init_app(app)
+        login_manager = LoginManager()
+        login_manager.login_view = 'auth.login_post'
+        login_manager.init_app(app)
+
+        # Create the following statement to retain the value in database
+        db.drop_all()
+
+        db.create_all()
+
+        @login_manager.user_loader
+        def load_user(user_id):
+            return User.query.get(int(user_id))
+        
+        from src.auth import auth
+        
+        app.register_blueprint(auth, url_prefix='/')
+        # app.register_blueprint(views, url_prefix='/')
+
+    return app
+
+
+__all__ = [
+    "create_app",
+    "db",
+    "User"
+]
