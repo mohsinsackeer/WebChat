5f2becf4d39488dad5906df191077b896b35e449
adwaithkrishnar <na19b053@smail.iitm.ac.in>
2022-07-08 07:51:08 +0530

    Implemented Chat window text message, left window lists added dynamically

--- src/static/js/chat.js
+++ src/static/js/chat.js
@@ -1,5 +1,36 @@
 var main = function(){
     console.log('chat.js is active!');
+
+    var names= ["Adwaith", "Mohsin","DK"];
+    var list = $('#chat-list');
+    var str = "https://png.pngitem.com/pimgs/s/150-1503945_transparent-user-png-default-user-image-png-png.png"
+    names.forEach((i)=>{
+        var li = $('<li>');
+        var div = $('<div>');
+        div.addClass("check_class");
+        div.prepend('<img src = "'+str+'"  alt="contact_image">');
+        div.append('<span >'+i+'</span>');
+        li.append(div);
+        list.append(li);
+    });
+
+    var send = $('#send_bt');
+    var msg_text =$('#msg_input');
+    var msgArray =[];
+    var messages = $('#messages');
+
+
+    send.on('click',function(){
+        if(msg_text.val()!=''){
+            msgArray.push(msg_text.val())
+            var li = $('<li>');
+            li.text(msg_text.val());
+            messages.append(li);
+            msg_text.val('');
+        }
+    });
 };
+
+
 
 $(document).ready(main);
