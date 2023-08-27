e96229966e5e202f59b95d8fb508c0ada5baf72f
M Mohsin Sackeer <mohsin.ms97@gmail.com>
2023-08-27 10:55:48 +0530

    Credentials transferred in config.properties file

--- src/cloudinary_creds.py
+++ src/cloudinary_creds.py
@@ -1,7 +1,12 @@
 import cloudinary
+from jproperties import Properties
 
-cloudinary.config( 
-  cloud_name = "mohsinsackeer", 
-  api_key = "136388425422833", 
-  api_secret = "RhJMYBYwe2VLBmRwi2nMR22KDgk" 
+configs = Properties()
+with open('src/configuration/config.properties', 'rb') as read_prop:
+  configs.load(read_prop)
+
+cloudinary.config(
+  cloud_name = configs.get("CLOUD_NAME").data,
+  api_key = configs.get("API_KEY").data,
+  api_secret = configs.get("API_SECRET").data
 )
